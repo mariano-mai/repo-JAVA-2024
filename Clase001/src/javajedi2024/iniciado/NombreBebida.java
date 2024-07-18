ebda42c5f50a8c14aad9dfbb3584561b1fb88d4f
Mariano <mariano.ramaidana@gmail.com>
2024-07-18 02:37:03 -0300

    001; java-jedi; nombre de bebida

---
+++ Clase001/src/javajedi2024/iniciado/NombreBebida.java
@@ -0,0 +1,14 @@
+package javajedi2024.iniciado;
+
+public class NombreBebida {
+
+	public static void main(String[] args) {
+		
+		String mascota = "Micha";
+		String pelicula = "Criminal";
+		
+		System.out.println("Nombre de la próxima bebida: "+mascota+" "+pelicula);
+
+	}
+
+}
