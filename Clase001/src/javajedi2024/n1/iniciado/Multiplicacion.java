252d71a0afab90409968e146d7a35d77c5154f20
Mariano <mariano.ramaidana@gmail.com>
2024-07-18 02:49:30 -0300

    004; java-jedi; multiplicacion.java

---
+++ Clase001/src/javajedi2024/n1/iniciado/Multiplicacion.java
@@ -0,0 +1,24 @@
+package javajedi2024.n1.iniciado;
+
+public class Multiplicacion {
+
+	public static void main(String[] args) {
+		
+		double valor1 = 3.543d;
+		double valor2 = 9.2d;
+		
+		double producto = valor1*valor2;
+		
+		int valor3 = 34;
+		int valor4 = 5;
+		
+		int producto2 = valor3*valor4;
+		
+		double suma = producto+producto2;
+		
+		System.out.println("Resultado:"+suma);
+		System.out.println("Va a ser real, porque parte de las variables que lo componen son reales.");
+
+	}
+
+}
