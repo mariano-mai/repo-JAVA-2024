3c0110ee80737e70d7d347c0839e7ee3ec91b0e8
Mariano <mariano.ramaidana@gmail.com>
2024-07-19 15:24:14 -0300

    java-jedi; guia n2, iniciado

---
+++ Clase001/src/javajedi2024/n2/iniciado/MisAmigos.java
@@ -0,0 +1,21 @@
+package javajedi2024.n2.iniciado;
+
+public class MisAmigos {
+
+	public static void main(String[] args) {
+		
+		String pers1 = "Francisco";
+		String pers2 = "Germán";
+		String pers3 = "Edgardo";
+		String pers4 = "Leandro";
+		String pers5 = "Rita";
+		String pers6 = "Evelyn";
+		String pers7 = "Aurora";
+		String pers8 = "Gisela";
+		
+		int i = 2;
+		String texto = pers1;
+
+	}
+
+}
---
+++ Clase001/src/javajedi2024/n2/iniciado/TrianguloDeAsteriscos.java
@@ -0,0 +1,16 @@
+package javajedi2024.n2.iniciado;
+
+public class TrianguloDeAsteriscos {
+
+	public static void main(String[] args) {
+		
+		String asterisco = "";
+		
+		for(int i=0; i<5; i++) {
+			asterisco = asterisco+"*";
+			System.out.println(asterisco);
+		}
+
+	}
+
+}
