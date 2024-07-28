35b5f8cce07542ea677f5a57e32ae68542978091
Mariano <mariano.ramaidana@gmail.com>
2024-07-28 17:41:34 -0300

    ejercicios nivel padawan, guia n2

---
+++ Clase001/src/javajedi2024/n2/padawan/ConviertoCosas.java
@@ -0,0 +1,12 @@
+package javajedi2024.n2.padawan;
+
+public class ConviertoCosas {
+
+	public static void main(String[] args) {
+		
+		//Demuestre cómo convertir un bucle WHILE en un DO WHILE.
+		
+
+	}
+
+}
