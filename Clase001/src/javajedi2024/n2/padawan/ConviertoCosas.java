3f7c5562d1655631cbe8e23f1ab42e3eed300835
Mariano <mariano.ramaidana@gmail.com>
2024-08-01 12:22:23 -0300

    ejercicios 1 y 2 de nivel padawan-n2

--- Clase001/src/javajedi2024/n2/padawan/ConviertoCosas.java
+++ Clase001/src/javajedi2024/n2/padawan/ConviertoCosas.java
@@ -4,8 +4,24 @@ public class ConviertoCosas {
 
 	public static void main(String[] args) {
 		
-		//Demuestre cómo convertir un bucle WHILE en un DO WHILE.
+		System.out.println("BUCLE WHILE");
 		
+		int contador = 0;
+		
+		while(contador <=5) {
+			System.out.println(contador);
+			contador++;
+		}
+		
+		contador = 0;
+		
+		System.out.println("BUCLE DO-WHILE");
+		
+		do {
+			System.out.println(contador);
+			contador++;
+		}
+		while(contador<=5);
 
 	}
 
---
+++ Clase001/src/javajedi2024/n2/padawan/ConviertoCosas2.java
@@ -0,0 +1,25 @@
+package javajedi2024.n2.padawan;
+
+public class ConviertoCosas2 {
+
+	public static void main(String[] args) {
+		
+		int contador = 0;
+		
+		System.out.println("BUCLE WHILE");
+		
+		while(contador <= 5) {
+			System.out.println(contador);
+			contador++;
+		}
+		
+		
+		System.out.println("BUCLE FOR");
+		
+		for(int i=0; i<=5; i++) {
+			System.out.println(i);
+		}
+
+	}
+
+}
